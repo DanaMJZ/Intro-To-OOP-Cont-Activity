3bd9eee8756ed5cc66d08c73d5c8573dda254d2d
Viktoriia <lyudkevych.v@gmail.com>
2025-04-02 11:18:22 -0700

    Cheking for pulled code and changes

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,3 +1,10 @@
 from school_schedule.student import Student
 
 # Write your tests here!
+def test_get_num_classes_nominal(self):
+    
+        student = Student("Claire", "freshmen", ["Gym", "Art", "Science"])
+    
+        count = student.get_num_classes()
+        # Assert
+        self.assertEqual(count, 3)
